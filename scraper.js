633cf357b116790ebc5a617261f8dd931d854fcc
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-01 15:03:30 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,59 +1,22 @@
-// This is a template for a Node.js scraper on morph.io (https://morph.io)
+var request = require('request');
+var async = require('async');
+var sqlite3 = require('sqlite3').verbose();
 
-var cheerio = require("cheerio");
-var request = require("request");
-var sqlite3 = require("sqlite3").verbose();
+function requestFunction(){
+    request('http://www.landkreis-heilbronn.de/zulassung/kfz.json', function (error, response, html) {
+        if (!error && response.statusCode == 200) {
 
-function initDatabase(callback) {
-	// Set up sqlite database.
-	var db = new sqlite3.Database("data.sqlite");
-	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
-		callback(db);
-	});
+            async.series([function (callback) {
+                data = JSON.parse(html);
+                callback();
+            }], function (err) {
+                statement.run(data[0].visitors, data[0].wait, data[0].nextcall, data[0].lastupdate);
+            });
+        }
+    });
 }
 
-function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
-}
-
-function readRows(db) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
-}
-
-function fetchPage(url, callback) {
-	// Use request to read in pages.
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
-		}
-
-		callback(body);
-	});
-}
-
-function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
-
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
-
-		readRows(db);
-
-		db.close();
-	});
-}
-
-initDatabase(run);
+var db = new sqlite3.Database('data.sqlite');
+db.exec("CREATE TABLE IF NOT EXISTS KFZ(visitors INT, wait INT, nextcall INT, lastupdate DATE PRIMARY KEY)");
+var data;
+var statement = db.prepare("INSERT OR IGNORE INTO KFZ VALUES (?, ?, ?, ?)");
